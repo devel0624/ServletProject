68bfbe4a3e0604fb880cb7ed3a8728d81cae44dd
Lee Seong Jun <devel0624@gmail.com>
2022-10-26 14:17:51 +0900

    edit FoodsServlet

--- src/main/java/com/nhnacademy/mart/servlet/FoodsServlet.java
+++ src/main/java/com/nhnacademy/mart/servlet/FoodsServlet.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.mart.servlet;
 
 import com.nhnacademy.mart.domain.FoodStand;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -20,14 +22,27 @@ public class FoodsServlet extends HttpServlet {
          */
         FoodStand stand = (FoodStand) request.getServletContext().getAttribute("foodStand");
 
+        Map<String , Integer> foodList = new HashMap<>();
+
         stand.getFoods().forEach(food -> {
-            try {
-                response.getWriter().println(
-                    food.getName() + " " + food.getPrice());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            if( !foodList.containsKey(food.getName())){
+                foodList.put(food.getName(),1);
+            } else {
+                foodList.replace(food.getName(), foodList.get(food.getName())+1);
             }
         });
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+
+        foodList.forEach(
+            (key,value)-> {
+                try {
+                    response.getWriter().println( key + " " + value + "개 있습니다.");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
     }
 
     @Override
