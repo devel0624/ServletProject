727bb0ebc0bc349910af981c800ac071ce4abfda
Lee Seong Jun <devel0624@gmail.com>
2022-10-26 13:55:10 +0900

    commit FoodsServlet

--- src/main/java/com/nhnacademy/mart/servlet/FoodsServlet.java
+++ src/main/java/com/nhnacademy/mart/servlet/FoodsServlet.java
@@ -18,7 +18,6 @@ public class FoodsServlet extends HttpServlet {
          * 생각보다 복잡할 수 있으므로 우선 상품 목록 출력을 한 다음
          * 그 이후에 장바구니에 담을 수 있는 form을 구성하는 순서로 진행을 추천
          */
-
         FoodStand stand = (FoodStand) request.getServletContext().getAttribute("foodStand");
 
         stand.getFoods().forEach(food -> {
